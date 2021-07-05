9da130ede35a57c51d391a387a5ecdfa32d0ea15
giotto-learn <maintainers@giotto.ai>
2021-07-05 14:47:02 +0200

    fix copyright date

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath(os.path.join('..','..')))
 # -- Project information -----------------------------------------------------
 
 project = 'giotto-ph'
-copyright = '2020, L2F SA'
+copyright = '2021, L2F SA'
 author = 'Julian Burella Perez'
 
 # The full version, including alpha/beta/rc tags
